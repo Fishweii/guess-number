76b17fdbfdc9aa274ec272f167840236223a0d67
wei Feng <fengweii1118@gmail.com>
2020-01-31 12:55:31 +0800

    add -f -all

--- js/guess.js
+++ js/guess.js
@@ -1,18 +1,44 @@
 var radomnum = Math.floor(Math.random() * 100) + 1;
 var guess = document.querySelector(".mynum");
-var answer = document.querySelector(".answer");
 var selectnum = document.querySelector(".go");
-var c = document.querySelector(".currect");
+var hiorlow = document.querySelector(".hiorlow");
+var recnum = document.querySelector(".pastnum");
+var reset = document.querySelector(".reset");
+var guesstimes = 1;
 
 function checkednum() {
     var guessnum = Number(guess.value);
     if (radomnum == guessnum) {
-        answer.innerHTML = ("恭喜答對！");
-
+        hiorlow.innerHTML = ("恭喜答對！");
+        gameover();
+    } else if (guesstimes == 10) {
+        hiorlow.innerHTML = ("遊戲結束");
+        gameover();
     } else {
-        answer.innerHTML = ("答案錯誤");
-        c.innerHTML = radomnum;
+        if (radomnum > guessnum) {
+            hiorlow.innerHTML = ("數字太小囉");
+        } else if (radomnum < guessnum) {
+            hiorlow.innerHTML = ("數字太大囉");
+        }
     }
 
+    recnum.innerHTML += ("&nbsp") + guessnum;
+    guesstimes++;
 }
 selectnum.addEventListener("click", checkednum);
+
+function gameover() {
+    selectnum.setAttribute("style", "display:none");
+    reset.setAttribute("style", "display:inline");
+}
+
+function resetgame() {
+    radomnum = Math.floor(Math.random() * 100) + 1;
+    guesstimes = 1;
+    guess.value = "";
+    reset.setAttribute("style", "display:none");
+    selectnum.setAttribute("style", "display:inline");
+    hiorlow.innerHTML = "";
+    recnum.innerHTML = "";
+}
+reset.addEventListener("click", resetgame);
